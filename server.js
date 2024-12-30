fe878503324147176a6cab6d5a08aeba6357655a
ShenoudaElfayez <tto1@kimo.com>
2024-12-31 01:25:23 +0200

    update server.js

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ app.get('/', (req, res) => {
   res.send('API is working!');
 });
 
+// مسار جديد لإرجاع المنتجات
+app.get('/api/products', (req, res) => {
+  const products = [
+    { id: 1998, name: 'Product 1', description: 'This is product 1' },
+    { id: 1999, name: 'Product 2', description: 'This is product 2' },
+  ];
+  res.json(products);
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
