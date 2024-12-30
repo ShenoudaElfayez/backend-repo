138ad0563d3177c22ccfebce77af7907e5e4d72a
ShenoudaElfayez <tto1@kimo.com>
2024-12-31 01:17:20 +0200

    update server.js

--- server.js
+++ server.js
@@ -1,24 +1,11 @@
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const productRoutes = require('./routes/productRoutes');
-const dotenv = require('dotenv');
 const app = express();
+const port = 3000;
 
-dotenv.config();
+app.get('/', (req, res) => {
+  res.send('API is working!');
+});
 
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Static folder for uploads
-app.use('/uploads', express.static('uploads'));
-
-// Routes
-app.use('/api/products', productRoutes);
-
-// Start server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
